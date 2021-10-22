7c427a74c2c6c4953ed6b7afd11ff7fa7ea6c10f
imdaebeen <imdaebeen_09@naver.com>
2021-10-23 02:30:42 +0900

    주석 정리 README 생성

--- post/views.py
+++ post/views.py
@@ -1,19 +1,3 @@
-# from rest_framework import serializers
-# from rest_framework.response import Response
-# from rest_framework.decorators import api_view
-# from .models import Post
-# from .serializers import PostSerializer
-
-# @api_view(['GET'])
-# def helloAPI(request):
-#     return Response("hello world!!")
-
-# @api_view(['GET'])
-# def postList(request):
-#     totalPost = Post.objects.all()
-#     serializers = PostSerializer(totalPost, many=True)
-#     return Response(serializers.data)
-
 from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from .models import Post
